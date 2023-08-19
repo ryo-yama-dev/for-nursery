11cbee7379e9eb414d9e62fe489c730ee32161a1
ryo-yama-dev <springwindwriter@gmail.com>
2023-08-19 20:12:29 +0900

    refactor: データ成型用関数追加

--- src/app/services/child.py
+++ src/app/services/child.py
@@ -1,5 +1,6 @@
 import strawberry
 
+from app.database import ChildModel
 from app.inputs import ChildCreateInput
 from app.repositories import ChildRepository
 from app.types import Child
@@ -10,16 +11,27 @@ __all__ = ["ChildService"]
 
 
 class ChildService(BaseService):
+    """
+    園児データの操作用ロジック
+    """
+
+    @classmethod
+    def _data_format(cls, data: ChildModel) -> Child:
+        """
+        ChildModel を Child に変換する
+        """
+        return Child(**data.to_dict())
+
     def find_all(self) -> list[Child]:
         return [
-            Child(**child.to_dict())
+            self._data_format(child)
             for child in ChildRepository(self.session).find_all()
         ]
 
     def find_one(self, id: int) -> Child | None:
         child = ChildRepository(self.session).find_by_id(id=id)
-        return Child(**child.to_dict()) if child else None
+        return self._data_format(child) if child else None
 
     def create(self, input: ChildCreateInput) -> Child:
         child = ChildRepository(self.session).create(strawberry.asdict(input))
-        return Child(**child.to_dict())
+        return self._data_format(child)
--- src/app/services/employee.py
+++ src/app/services/employee.py
@@ -1,5 +1,6 @@
 import strawberry
 
+from app.database import EmployeeModel
 from app.inputs import EmployeeCreateInput
 from app.repositories import EmployeeRepository
 from app.types import Employee, Job, Profile
@@ -15,28 +16,27 @@ class EmployeeService(BaseService):
     従業員操作のための業務ロジック
     """
 
+    @classmethod
+    def _data_format(cls, data: EmployeeModel) -> Employee:
+        """
+        EmployeeModel を Employee に変換する
+        """
+        return Employee(
+            **data.to_dict(),
+            job=Job(**data.job.to_dict()),
+            profiles=[Profile(**prof.to_dict()) for prof in data.profiles],
+        )
+
     def find_all(self) -> list[Employee]:
         return [
-            Employee(
-                **employee.to_dict(),
-                job=Job(**employee.job.to_dict()),
-                profiles=[Profile(**prof.to_dict()) for prof in employee.profiles],
-            )
+            self._data_format(employee)
             for employee in EmployeeRepository(self.session).find_all()
         ]
 
     def find_one(self, id: int) -> Employee | None:
         employee = EmployeeRepository(self.session).find_by_id(id=id)
-        return (
-            Employee(
-                **employee.to_dict(),
-                job=Job(**employee.job.to_dict()),
-                profiles=[Profile(**prof.to_dict()) for prof in employee.profiles],
-            )
-            if employee
-            else None
-        )
+        return self._data_format(employee) if employee else None
 
     def create(self, input: EmployeeCreateInput) -> Employee:
         employee = EmployeeRepository(self.session).create(strawberry.asdict(input))
-        return Employee(**employee.to_dict(), job=Job(**employee.job.to_dict()))
+        return self._data_format(employee)
